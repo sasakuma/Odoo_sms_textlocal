88898664eda2ab0a11d4ef5b3553eaecd5312b0f
Senthilnathan G <bornspy@gmail.com>
2018-11-01 17:23:20 +0530

    text local module added

---
+++ sms_textlocal/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'SMS gateway',
+    'category': 'Tools',
+    'summary': 'SMS Text Messaging',
+    'description': """
+This module gives a framework for SMS text messaging
+----------------------------------------------------
+
+The service is provided by the In App Purchase Odoo platform.
+""",
+    'depends': ['sms'],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
---
+++ sms_textlocal/models/sms_api.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+from odoo.addons.iap.models import iap
+import requests
+DEFAULT_ENDPOINT = 'https://api.textlocal.in'
+DEFAULT_SENDER = 'INNOVIK'
+
+
+class SmsApi(models.AbstractModel):
+    _inherit = 'sms.api'
+    @api.model
+    def _send_sms(self, numbers, message):
+        """ Send sms
+        """
+        apikey = self.env['iap.account'].get('textlocal')
+        sender = self.env['ir.config_parameter'].sudo().get_param('textlocal.sender', DEFAULT_SENDER)
+        params = {
+            'apikey': apikey.account_token, 
+			'message' : message,
+			'sender': sender,
+			'numbers': numbers
+        }
+        endpoint = self.env['ir.config_parameter'].sudo().get_param('textlocal.endpoint', DEFAULT_ENDPOINT)
+        url = endpoint + '/send/?'
+        r = requests.post(url=url, params=params)
+        return True
+        # ~ r = iap.jsonrpc(endpoint + '/send/?', params=params)
+        # ~ print (params)
+        # ~ print (endpoint)
+        # ~ print (sender)
+        # ~ print (apikey.account_token)
+        # ~ print (url)
+        # ~ print (r.status_code)
+        # ~ print (r.text)
+        
